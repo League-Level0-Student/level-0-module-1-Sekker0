f04b10908f8b78e95b62d039731aa2c92cd0e66e
league <alishahussain1229@gmail.com>
2023-01-30 18:19:42 -0800

    qwertyuiopplkjhgfdsaazxcvbnm

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,11 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+	String response = JOptionPane.showInputDialog("What Are You Good At?");
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+	String question = JOptionPane.showInputDialog("Please Enter Your Name Here");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+	JOptionPane.showMessageDialog(null, "hey " + question + " You Are Good At" + response+ "!");
 	}
 }
 
--- src/_03_if_else/_4_tasty_tomato/tasty_tomato/tasty_tomato.pde
+++ src/_03_if_else/_4_tasty_tomato/tasty_tomato/tasty_tomato.pde
@@ -1,10 +1,19 @@
+int bite = 0;
+
 void setup() {
     size(500, 500);
 }
 void draw() {
     background(200, 200, 200);
     noStroke();
+    fill(0, 200, 0);
+   rect(176, 103, 12, 32);
+    fill( 200, 0, 0);
     ellipse(150, 200, 150, 150);
     ellipse(212, 200, 150, 150);
-    rect(176, 103, 12, 32);
+  if (mousePressed){
+    bite = 50;
+  }
+   fill(200, 200, 200);
+   ellipse(85, 200, bite, bite);
 }
